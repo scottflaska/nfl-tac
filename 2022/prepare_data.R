cc9f3be09eafc243a5e50930a2aacfbd6687ab15
Scott Flaska <scott.flaska@gmail.com>
2022-05-05 12:11:26 -0600

    updated slides and commented scripts

--- 2022/prepare_data.R
+++ 2022/prepare_data.R
@@ -1,10 +1,18 @@
+#Load libraries
 suppressPackageStartupMessages(library(tidyverse))
 
 library(readr)
 
+#Load training data 
+#Source: Kaggle (https://www.kaggle.com/competitions/tabular-playground-series-may-2022/data)
+#Prompt: For this challenge, you are given (simulated) manufacturing control data and are tasked to predict whether the machine is in state 0 or state 1.
 train = read_csv("~/project/2022/data/train.csv",show_col_types = F)
 
-model_data = train %>% 
+#View the structure of the training data
+# train %>% str(give.attr = F)
+
+#Select subset of features (columns) from the dataset
+train_feature_subset = train %>% 
   select(id, 
          f_00,
          f_01,
@@ -14,17 +22,16 @@ model_data = train %>%
          f_05,
          f_06,
          f_07,
-         f_08,
-         target)
+         target) 
 
-model_data %>% head()
+#Split data into a training fold and a validation fold
+train_fold = train_feature_subset %>% 
+  filter(id < 100000)
 
-train_fold = model_data %>% 
-  filter(id < 300000)
-
-validation_fold = model_data %>% 
+validation_fold = train_feature_subset %>% 
   filter(!(id %in% train_fold$id))
 
+#Save the prepared data
 saveRDS(object = train_fold, file = "~/project/2022/outputs/train_fold.rds")
 
 saveRDS(object = validation_fold, file = "~/project/2022/outputs/validation_fold.rds")
--- 2022/train_model.R
+++ 2022/train_model.R
@@ -1,11 +1,14 @@
+#Load libraries
 suppressPackageStartupMessages(library(tidyverse))
 
 library(rpart)
 
+#Load the prepared data 
 train_fold = readRDS("~/project/2022/outputs/train_fold.rds")
 
 validation_fold = readRDS("~/project/2022/outputs/validation_fold.rds")
 
+#Train a decision tree model using the training fold
 rpart_model = rpart(formula = target ~ . - id,
                     data = train_fold,
                     method = "class",
@@ -13,12 +16,16 @@ rpart_model = rpart(formula = target ~ . - id,
 
 saveRDS(rpart_model,file = "~/project/2022/outputs/model.rds")
 
+#Use model to make predictions on validation fold
 validation_preds = validation_fold
 
 validation_preds$pred = predict(object = rpart_model,
                                 newdata = validation_fold,
                                 type = "class")
+#View the prediction column
+# validation_preds %>% head()
 
+#Measure the accuracy of the models prediction on the validation set
 validation_performance = validation_preds %>% 
   mutate(correct = ifelse(pred == target,1,0))
 
@@ -27,5 +34,5 @@ accuracy = validation_performance %>%
   pull(accuracy) %>% 
   round(digits = 4)
 
+#Save the accuracy value as a text file
 cat(accuracy,file = "~/project/2022/accuracy.txt")
-
